cca9110fa2809b186c75752de584f5e1e6b834f4
Neido <reg@neido.de>
2023-05-07 20:09:46 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -8,14 +8,18 @@ OPTIONAL="discord vlc steam signal-desktop remmina bitwarden lutris virt-manager
 
 # Greeter
 echo "Welcome to the install script!"
-
+echo ""
 echo "This script will..."
 echo "  1. Run a full system update."
 echo "  2. Make sure yay is installed."
-echo "  3. Install dependencies."
+echo "  3. Install requirements."
 echo "  4. Install the dotfiles."
+echo "  5. Install optional packages."
+echo "  5. Cleanup."
+echo ""
 
 read -n 1 -s -r -p "Press any key when ready..."
+echo ""
 
 # Makes sure yay is intalled, as it is needed for this script
 ensure_yay() {
@@ -35,20 +39,7 @@ install_if_needed() {
 	for package in $REQ; do
 		if ! yay -Qs $package >/dev/null; then
 			echo "Installing $package"
-			yay -S --needed $package
-		else
-			echo "$package is already installed"
-		fi
-	done
-}
-
-# Installs selected applications
-install_applications() {
-	local REQ="$@"
-	for package in $REQ; do
-		if ! yay -Qs $package >/dev/null; then
-			echo "$package is not installed. Installing $package..."
-			yay -S --needed $package
+			yay -S --noconfirm --needed $package
 		else
 			echo "$package is already installed"
 		fi
@@ -60,17 +51,25 @@ update_folder() {
 	local folder="$1"
 	local name="$2"
 	if [ -d ~/$folder ]; then
-		echo "$name configs detected, backing up..."
-		if ! [ -d ~/$folder.old ]; then
-			mkdir ~/$folder.old
+		if [ $backup = "yes" ]; then
+			echo "$name configs detected, backing up..."
+			if [ -d ~/$folder.old ]; then
+				rm -rf ~/$folder.old
+				mkdir ~/$folder.old
+			else
+				mkdir ~/$folder.old
+			fi
+			mv ~/$folder/* ~/$folder.old/
+		else
+			echo "$name configs detected, deleting..."
+			rm -rf ~/$folder
+			mkdir ~/$folder
 		fi
-		mv ~/$folder/* ~/$folder.old/
-		cp -r ./dots/dotfiles/$folder/* ~/$folder
 	else
-		echo "Installing $name configs..."
 		mkdir ~/$folder
-		cp -r ./dots/dotfiles/$folder/* ~/$folder
 	fi
+	echo "Installing $name configs..."
+	cp -r ./dots/dotfiles/$folder/* ~/$folder
 }
 
 # Backup current settings and copy new ones
@@ -78,14 +77,34 @@ update_file() {
 	local file="$1"
 	local name="$2"
 	if [ -f ~/$file ]; then
-		echo "$name config detected, backing up..."
-		mv ~/$file ~/$file.old
-		cp ./dots/dotfiles/$file ~/$file
+		if [ $backup = "yes" ]; then
+			echo "$name config detected, backing up..."
+			mv ~/$file ~/$file.old
+		else
+			rm -rf ~/$file
+			mkdir ~/$file
+		fi
 	else
 		echo "Installing $name configs..."
-		cp ./dots/dotfiles/$file ~/$file
 	fi
+	cp ./dots/dotfiles/$file ~/$file
 }
+
+# Settings
+echo ""
+read -r -p ":: Do you want to backup old configs? [Y/n]: " backup
+
+case $backup in
+[yY][eE][sS] | [yY])
+	backup="yes"
+	;;
+[nN][oO] | [nN])
+	backup="no"
+	;;
+[*])
+	backup="yes"
+	;;
+esac
 
 # 1.
 echo ""
@@ -149,28 +168,49 @@ update_folder ".config/qutebrowser" "Qutebrowser"
 update_folder ".config/rofi" "Rofi"
 update_folder ".config/awesome" "Awesome"
 
+# 5.
 echo ""
 echo "==================================================================="
 echo "Installing optional packages..."
 echo "==================================================================="
 echo ""
 
-echo "Packages ($(echo $APPLICATIONS | wc -w)) $APPLICATIONS"
+read -r -p ":: Do you want to install liblua_pam for the lockscreen? [Y/n]: " pam
 echo ""
 
-read -r -p ":: Do you want to install my recommended packages? [Y/n]: " opt
-echo ""
-
-case $opt in
-[yY][eE][sS]|[yY])
-	install_if_needed $APPLICATIONS;;
-[nN][oO]|[nN])
-	continue;;
+case $pam in
+[yY][eE][sS] | [yY])
+	install_if_needed "lua-pam-git"
+	cp /usr/lib/lua-pam/liblua_pam.so ~/.config/awesome/
+	;;
+[nN][oO] | [nN])
+	continue
+	;;
 [*])
-	install_if_needed $APPLICATIONS;;
+	install_if_needed "lua-pam-git"
+	cp /usr/lib/lua-pam/liblua_pam.so ~/.config/awesome/
+	;;
 esac
 
 echo ""
+echo "Packages ($(echo $APPLICATIONS | wc -w)) $APPLICATIONS"
+echo ""
+
+read -r -p ":: Do you want to install my recommended packages? [Y/n]: " rec
+echo ""
+
+case $rec in
+[yY][eE][sS] | [yY])
+	install_if_needed $APPLICATIONS
+	;;
+[nN][oO] | [nN])
+	continue
+	;;
+[*])
+	install_if_needed $APPLICATIONS
+	;;
+esac
+
 echo ""
 echo "Packages ($(echo $OPTIONAL | wc -w)) $OPTIONAL"
 echo ""
@@ -179,15 +219,18 @@ read -r -p ":: Do you want to install optional packages? [Y/n]: " opt
 echo ""
 
 case $opt in
-[yY][eE][sS]|[yY])
-	install_if_needed $OPTIONAL;;
-[nN][oO]|[nN])
-	continue;;
+[yY][eE][sS] | [yY])
+	install_if_needed $OPTIONAL
+	;;
+[nN][oO] | [nN])
+	continue
+	;;
 [*])
-	install_if_needed $OPTIONAL;;
+	install_if_needed $OPTIONAL
+	;;
 esac
 
-# 5.
+# 6.
 echo ""
 echo "==================================================================="
 echo "Finishing up..."
@@ -198,5 +241,16 @@ echo "Cleaning temp files..."
 rm -rf ./dots >/dev/null
 
 echo ""
-echo ""
 echo "Installation complete!"
+echo ""
+
+if pgrep awesome >/dev/null; then
+	echo "Restart AwesomeWM using the keybind: CRTL + SUPER (Windows) + R"
+else
+	echo "Starting AwesomeWM..."
+	if [[ $(tty) = /dev/tty1 ]]; then
+		startx
+	else
+		echo "Not in tty1. Start AwesomeWM manually using: startx"
+	fi
+fi
